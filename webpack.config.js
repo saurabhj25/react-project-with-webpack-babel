1857d51e173db34511f872f7181672c79f3d832a
saurabhj25 <saurabhj.25a@gmail.com>
2023-09-09 03:12:23 +0530

    add pre commit linting&prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "static/js/[name].[hash:8].js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'static/js/[name].[hash:8].js',
   },
   devServer: {
     port: 3000,
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       minify: {
         removeComments: true,
         minifyJS: true,
@@ -39,23 +39,23 @@ module.exports = {
     }),
   ],
   resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: require.resolve("babel-loader"),
+        loader: require.resolve('babel-loader'),
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"], //style-loader (2.inject css into dom) css-loader(1.add css to js)
+        use: ['style-loader', 'css-loader'], //style-loader (2.inject css into dom) css-loader(1.add css to js)
       },
       {
         test: /\.png|svg|jpg|gif$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,15 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "static/js/[name].[hash:8].js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'static/js/[name].[hash:8].js',
   },
   devServer: {
     port: 3000,
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       minify: {
         removeComments: true,
         minifyJS: true,
@@ -43,33 +43,33 @@ module.exports = {
       },
     }),
     new MiniCssExtractPlugin({
-      filename: "static/css/[name].[hash:8].css",
-      chunkFilename: "static/css/[name].[hash:8].css",
+      filename: 'static/css/[name].[hash:8].css',
+      chunkFilename: 'static/css/[name].[hash:8].css',
       ignoreOrder: false,
     }),
   ],
   resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: require.resolve("babel-loader"),
+        loader: require.resolve('babel-loader'),
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"], //style-loader (2.extract css into files) css-loader(1.add css to js)
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], //style-loader (2.extract css into files) css-loader(1.add css to js)
       },
       {
         test: /\.png|svg|jpg|gif$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "static/media/[name].[hash:8].[ext]",
+              name: 'static/media/[name].[hash:8].[ext]',
             },
           },
         ],
